94afd0bba8da5672ff337654b4e6340a7c126d60
calibregtx <85536934+calibregtx@users.noreply.github.com>
2022-12-25 15:44:54 +0700

    Update AWS.tf

--- AWS.tf
+++ AWS.tf
@@ -1,1 +1,36 @@
+terraform {
+  cloud {
+    organization = "calibre90"
+
+    workspaces {
+      name = "Randy_Local"
+    }
+  }
+}
+
+provider "aws" {
+    region = "ap-northeast-3"
+ 
+}
+
+
+
+resource "aws_default_vpc" "RA_VPC" {
+  tags = {
+    Name = "RA_VPC"
+
+  }
+}
+
+# EC2
+# resource "aws_instance" "Randy_web" {
+#   ami           = "ami-0bba69335379e17f8"
+#   instance_type = "t2.micro"
+# }
+# LB
+# resource "aws_lb" "RA_GWLB" {
+#   name = "RAGWLB"
+#   load_balancer_type = "gateway"
+  
+# }
 
