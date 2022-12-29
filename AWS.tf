a35599f5cc5e5f319164e22969bf01335ac81a2d
randy <randy@randiard9)@gmail.com>
2022-12-29 20:20:14 +0700

    ww

--- AWS.tf
+++ AWS.tf
@@ -15,18 +15,18 @@ provider "aws" {
 
 
 
-#  resource "aws_default_vpc" "RA_VPC" {
-#    tags = {
-#     Name = "RA_VPC"
-#   }
-#  }
-#}
-# EC2
-# resource "aws_instance" "Randy_web" {
-#   ami           = "ami-0bba69335379e17f8"
-#   instance_type = "t2.micro"
-# }
-# LB
+resource "aws_vpc" "RA_VPC" {
+  tags = {
+    Name = "RA_VPC"
+   }
+  }
+
+
+resource "aws_instance" "Randy_web" {
+  ami           = "ami-0bba69335379e17f8"
+  instance_type = "t2.micro"
+}
+
 # resource "aws_lb" "RA_GWLB" {
 #   name = "RAGWLB"
 #   load_balancer_type = "gateway"
